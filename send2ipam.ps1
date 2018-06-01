d5eb41c726bf6e49c80f6edbe9052c5d97107290
Rafael Ângelo de Souza Carneiro <rafaelangelo@live.com>
2018-06-01 15:34:02 +0100

    Send and Get reservations - IPAM
    Send reservations to IPAM and Get reservations from IPAM

---
+++ send2ipam.ps1
@@ -0,0 +1,206 @@
+﻿#==================================================
+#
+#....... Enviar Reservas para o IPAM ..............
+#
+#==================================================
+
+
+
+#-------------------------------------------
+# Server.DOMINIO / Ex.: DC.yourdomain.local
+
+$Domain = "DHCP-DEV-APP"
+
+#Scope(s) DHCP em decimal - Ex.: 192.168.200.0 = 3232286720 (Google: Convert ip to decimal)
+$scopes = {3232286720,3232261376}
+
+# Query to get all IPs
+$Query = "SELECT `ipaddresses`.ip_addr, `ipaddresses`.mac, `ipaddresses`.description, subnets.subnet FROM `ipaddresses` INNER JOIN subnets ON `ipaddresses`.`subnetId` = subnets.id WHERE subnets.subnet IN (" + $scopes + ") "
+
+#database and credentials
+$MySQLAdminUserName = 'dhcpsync' 
+$MySQLAdminPassword = '********' 
+$MySQLDatabase = 'phpipam' 
+$MySQLHost = '192.168.1.100' 
+
+$ConnectionString = "server=" + $MySQLHost + ";port=3306;uid=" + $MySQLAdminUserName + ";pwd=" + $MySQLAdminPassword + ";database="+$MySQLDatabase 
+
+
+#---------------------------------
+#Binary to IP Address
+#
+function IpFromBin($addressInBin){ 
+   [string[]]$addressInInt32 = @()
+   $addressInBin = $addressInBin.ToCharArray()
+   for ($i = 0;$i -lt $addressInBin.length;$i++) { 
+       $partAddressInBin += $addressInBin[$i]  
+       if(($i+1)%8 -eq 0){ 
+         $partAddressInBin = $partAddressInBin -join "" 
+         $addressInInt32 += [Convert]::ToInt32($partAddressInBin -join "",2)
+         $partAddressInBin = "" 
+            } 
+     }
+    $addressInInt32 = $addressInInt32 -join "." 
+    return $addressInInt32
+} 
+#------------------------------------
+
+#Converte IP (String) para Decimal
+#
+function IpToDec($ip){ 
+    
+    [String[]] $ip_array = $ip.Split('.')
+
+    $decimal = [Int64](([Int32])::Parse($ip_array[0])*[Math]::Pow(2,24)+
+            ([Int32])::Parse($ip_array[1])*[Math]::Pow(2,16)+
+            ([Int32])::Parse($ip_array[2])*[Math]::Pow(2,8)+
+            ([Int32])::Parse($ip_array[3]))
+    return $decimal
+} 
+#-----------
+#Converte IP (String) para Decimal / SUbnet
+#
+function IpToDecSubnet($ip){ 
+    
+    [String[]] $ip_array = $ip.Split('.')
+
+    $decimal = [Int64](([Int32])::Parse($ip_array[3])*[Math]::Pow(2,24)+
+            ([Int32])::Parse($ip_array[2])*[Math]::Pow(2,16)+
+            ([Int32])::Parse($ip_array[1])*[Math]::Pow(2,8)+
+            ([Int32])::Parse($ip_array[0]))
+    return $decimal
+} 
+#------------------------------------
+
+
+
+#Check IP on DHCP
+Write-Output "Check DHCP reservations.."
+  
+$checkReservations = 'Get-DhcpServerv4Scope -ComputerName ' + $Domain + ' | Get-DhcpServerv4Reservation -ComputerName '+ $Domain 
+Write-Output $checkReservations
+Invoke-Expression $checkReservations -OutVariable ondhcp
+
+#Write-Output "Result:"
+#Write-Output $ondhcp
+
+#++++++++++++++++++++++++++++++++++++
+# ExecuCao da query e atualizaCao das reservas no DHCP
+#------------------------------------
+Try { 
+ [void][System.Reflection.Assembly]::LoadWithPartialName("MySql.Data") 
+  $Connection = New-Object MySql.Data.MySqlClient.MySqlConnection 
+  $Connection.ConnectionString = $ConnectionString 
+  $Connection.Open() 
+  Write-Output "Running query..."
+  $Command = New-Object MySql.Data.MySqlClient.MySqlCommand($Query, $Connection) 
+  $DataAdapter = New-Object MySql.Data.MySqlClient.MySqlDataAdapter($Command) 
+  $DataSet = New-Object System.Data.DataSet 
+  $RecordCount = $dataAdapter.Fill($dataSet, "data") 
+  $Records = $DataSet.Tables[0]
+  $Records.Keys | ForEach-Object {$Records.$_}
+  #Write-Output $Records
+
+
+#++++++++++ Checking DHCP reservations  ++++++++++++++++
+  Write-Output " "
+  Write-Output " "
+  Write-Output "Checking DHCP reservations:"
+  foreach ($r_dhcp in $ondhcp){
+    Write-Output " "
+    $onipam = 0
+    $ip1 = [Convert]::ToString($r_dhcp.IPAddress)
+    
+    foreach ($r_ipam in $Records ){
+        $ip_addr = $r_ipam.ip_addr
+        $subnet = $r_ipam.subnet
+        $ip_addr= [Convert]::ToString($ip_addr,2)
+        $subnet = [Convert]::ToString($subnet,2)
+        $ip =  IpFromBin($ip_addr)
+        $scope =  IpFromBin($subnet)
+        $mac = $r_ipam.mac
+        $mac = [Convert]::ToString($mac)
+        $mac = $mac.Replace(':',"")           
+        
+        $ip2 = [Convert]::ToString($ip)
+        
+        if ($ip1.Equals($ip2)){
+            Write-Output $ip1
+             Write-Output "="
+              Write-Output $ip2
+            $onipam = 1
+            break;
+        }        
+    }
+    Write-Output "Scope:"
+    Write-Output $scope     
+    $subnet =  [IPAddress]::Parse($r_dhcp.ScopeId.Address.ToString())
+    $subnetip = $subnet
+    $subnet = $subnet.ToString()
+    $subnet = IpToDecSubnet($subnet)
+    
+    $ip1dec = IpToDec($ip1)
+    Write-Output $ip1dec
+    
+    if($onipam){
+        Write-Output $ip1
+        Write-Output "Found on IPAM"
+        
+    }else{
+        $ip_addr = $ip1dec
+        $mac = $r_dhcp.ClientId
+        $description = $r_dhcp.Description
+        $name = $r_dhcp.Name
+
+        Write-Output $ip1
+        Write-Output "Not found on IPAM"
+        Try{
+            $Query_subnet = "SELECT id FROM `subnets` WHERE subnets.subnet = " + $subnet + " "
+           # Write-Output $Query_subnet
+            Write-Output "Sending to IPAM..."
+            $Command = New-Object MySql.Data.MySqlClient.MySqlCommand($Query_subnet, $Connection) 
+            $DataAdapter = New-Object MySql.Data.MySqlClient.MySqlDataAdapter($Command) 
+            $DataSet = New-Object System.Data.DataSet 
+            $RecordCount = $dataAdapter.Fill($dataSet, "data") 
+            $Records = $DataSet.Tables[0]
+            $subnet = $Records[0].id
+             
+            $subnetcheck = $subnet.ToString()
+
+            if(![string]::IsNullOrEmpty($subnetcheck)){
+            # Write-Output $r_dhcp
+                $Query_insert = "INSERT INTO `ipaddresses` (`subnetId`, `ip_addr`, `description`, `dns_name`, `mac`) VALUES("+ $subnet +", '"+ $ip_addr +"','"+ $description +"','"+ $name +"','"+ $mac +"')"
+               # Write-Output $Query_insert
+                $Command = New-Object MySql.Data.MySqlClient.MySqlCommand($Query_insert, $Connection) 
+                $Command.ExecuteNonQuery()
+                Write-Output "Saved!"               
+             }
+             else{
+                Write-Output "Subnet:"
+                Write-Output $subnetip
+                #Write-Output $subnet
+                Write-Output "Subnet do not exist on IPAM. Register the scope/subnet on IPAM before run this script!!"
+            }
+        }
+        Catch{
+           Write-Host "ERROR : $query `n$Error[0]"
+        }
+        
+        #$rm =  'Remove-DhcpServerv4Reservation -ComputerName '+ $Domain + ' -IpAddress '+ $ip1 +''
+        #Invoke-Expression $rm
+        #Write-Output "Reservation REMOVED from DHCP."
+    }
+    Write-Output " "
+  }
+
+
+  
+  Write-Output "Finish."
+} 
+Catch { 
+    Write-Host "ERROR : $query `n$Error[0]" 
+} 
+
+Finally { 
+  $Connection.Close() 
+} 
---
+++ syncipam.ps1
@@ -0,0 +1,151 @@
+﻿param(
+[string]$ipAddr,
+[string]$subnetId,
+[string]$user
+)
+
+#==================================================
+#
+#.............. IPAM/DHCP SYNC ....................
+#
+#==================================================
+$date = Get-Date
+Write-Output "-----------------------------------------------------------------------------" >> C:\ipam\logdc01.txt
+Write-Output "Starting..." >> C:\ipam\logdc01.txt
+Write-Output $user >> C:\ipam\logdc01.txt
+Write-Output $date >> C:\ipam\logdc01.txt
+
+
+#-------------------------------------------
+# Server.DOMINIO / Ex.: DC.yourdomain.local
+
+$Domain = "DHCP-DEV-APP"
+
+
+
+#Converte IP (String) para Decimal
+#
+function IpToDec($ip){ 
+    
+    [String[]] $ip_array = $ip.Split('.')
+
+    $decimal = [Int64](([Int32])::Parse($ip_array[0])*[Math]::Pow(2,24)+
+            ([Int32])::Parse($ip_array[1])*[Math]::Pow(2,16)+
+            ([Int32])::Parse($ip_array[2])*[Math]::Pow(2,8)+
+            ([Int32])::Parse($ip_array[3]))
+    return $decimal
+}
+
+
+
+$ipAddrDec = IpToDec($ipAddr)
+
+# Query to get IPs from IPAM database
+$Query = "SELECT `ipaddresses`.ip_addr, `ipaddresses`.mac, `ipaddresses`.description, `subnets`.subnet FROM `ipaddresses` INNER JOIN `subnets` ON `ipaddresses`.subnetId = `subnets`.id WHERE `ipaddresses`.subnetId = "+ $subnetId+" AND  `ipaddresses`.ip_addr= "+ $ipAddrDec+" "
+
+#database and credentials
+$MySQLAdminUserName = 'dhcpsync' 
+$MySQLAdminPassword = '********' 
+$MySQLDatabase = 'phpipam' 
+$MySQLHost = '192.168.1.100' 
+$ConnectionString = "server=" + $MySQLHost + ";port=3306;uid=" + $MySQLAdminUserName + ";pwd=" + $MySQLAdminPassword + ";database="+$MySQLDatabase 
+
+
+
+#---------------------------------
+#Converte Binario para EndereCo IP
+#
+function IpFromBin($addressInBin){ 
+   [string[]]$addressInInt32 = @()
+   $addressInBin = $addressInBin.ToCharArray()
+   for ($i = 0;$i -lt $addressInBin.length;$i++) { 
+       $partAddressInBin += $addressInBin[$i]  
+       if(($i+1)%8 -eq 0){ 
+         $partAddressInBin = $partAddressInBin -join "" 
+         $addressInInt32 += [Convert]::ToInt32($partAddressInBin -join "",2)
+         $partAddressInBin = "" 
+            } 
+     }
+    $addressInInt32 = $addressInInt32 -join "." 
+    return $addressInInt32
+} 
+#------------------------------------
+
+#++++++++++++++++++++++++++++++++++++
+# ExecuCao da query e atualizaCao das reservas no DHCP
+#------------------------------------
+Try { 
+ [void][System.Reflection.Assembly]::LoadWithPartialName("MySql.Data") 
+  $Connection = New-Object MySql.Data.MySqlClient.MySqlConnection 
+  $Connection.ConnectionString = $ConnectionString 
+  $Connection.Open() 
+  Write-Output "Running query..."
+  $Command = New-Object MySql.Data.MySqlClient.MySqlCommand($Query, $Connection) 
+  $DataAdapter = New-Object MySql.Data.MySqlClient.MySqlDataAdapter($Command) 
+  $DataSet = New-Object System.Data.DataSet 
+  $RecordCount = $dataAdapter.Fill($dataSet, "data") 
+  $Records = $DataSet.Tables[0]
+  $Records.Keys | ForEach-Object {$Records.$_}
+  Write-Output $Records
+
+  foreach ($reservation in $Records ){
+    
+    Write-Output "Start check..."
+    
+    #Setting Values
+    $ip_addr = $reservation.ip_addr
+    $subnet = $reservation.subnet
+  
+    $ip_addr= [Convert]::ToString($ip_addr,2)
+    $subnet = [Convert]::ToString($subnet,2)
+
+    $ip =  IpFromBin($ip_addr)
+    $scope =  IpFromBin($subnet)
+
+    Write-Output $scope >> C:\ipam\log.txt
+    $mac = $reservation.mac
+    
+    $mac = [Convert]::ToString($mac)
+    $mac = $mac.Replace(':',"")
+    $mac = $mac.Replace('-',"")
+    $description = $reservation.description
+
+    #Check IP on DHCP
+    Write-Output "Check DHCP reservations.." >> C:\ipam\log.txt
+  
+    $checkReservations = 'Get-DhcpServerv4Reservation -ComputerName '+ $Domain + ' -IpAddress ' + $ip 
+    #Write-Output $checkReservations
+    $found = Invoke-Expression $checkReservations
+  
+    if(-not $found){
+        Write-Output "New reservation to load..." >> C:\ipam\log.txt
+        Write-Output "Loading dhcp Reservation"
+        Write-Output $ip >> C:\ipam\log.txt
+        $add =  'Add-DhcpServerv4Reservation -ScopeId '+ $scope+ ' -IpAddress '+ $ip +' -ClientId "'+ $mac +'" -Description "'+$description+'" '
+        Invoke-Expression $add | Tee-Object C:\ipam\log.txt
+    }  
+    
+    if($found){
+        Write-Output "Updating DHCP Reservation" >> C:\ipam\log.txt
+        Write-Output $ip >> C:\ipam\log.txt
+        $rm =  'Remove-DhcpServerv4Reservation -ComputerName '+ $Domain + ' -IpAddress '+ $ip +''
+        Invoke-Expression $rm | Tee-Object C:\ipam\log.txt
+        $add =  'Add-DhcpServerv4Reservation -ScopeId '+ $scope+ ' -IpAddress '+ $ip +' -ClientId "'+ $mac +'" -Description "'+$description+'" '
+        Invoke-Expression $add | Tee-Object C:\ipam\log.txt
+    }
+  }
+
+  Write-Output " . " >> C:\ipam\log.txt
+
+    
+  Write-Output "Finish." >> C:\ipam\log.txt
+
+} 
+Catch { 
+    
+    Write-Host "ERROR : $query `n$Error[0]" >> C:\ipam\log.txt
+} 
+
+Finally { 
+  $Connection.Close() 
+}
