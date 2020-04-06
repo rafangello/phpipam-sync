f7387407f3693d452621926bd6df294ca1f4ca44
Rafael Ângelo Carneiro <rafaelangelo@live.com>
2020-04-06 11:04:53 +0100

    2020 Update

--- syncipam.ps1
+++ syncipam.ps1
@@ -1,4 +1,4 @@
-﻿param(
+ param(
 [string]$ipAddr,
 [string]$subnetId,
 [string]$user
@@ -10,16 +10,17 @@
 #
 #==================================================
 $date = Get-Date
-Write-Output "-----------------------------------------------------------------------------" >> C:\ipam\logdc01.txt
-Write-Output "Starting..." >> C:\ipam\logdc01.txt
-Write-Output $user >> C:\ipam\logdc01.txt
-Write-Output $date >> C:\ipam\logdc01.txt
+Write-Output "-------------------------BEGIN-----------------------------" >> C:\ipam\log.txt
+Write-Output "Starting..." >> C:\ipam\log.txt
+Write-Output $user >> C:\ipam\log.txt
+Write-Output $date >> C:\ipam\log.txt
+Write-Output $ipAddr >> C:\ipam\log.txt
+Write-Output $subnetId >> C:\ipam\log.txt
+Write-Output "Loading variables..." >> C:\ipam\log.txt
 
-
-#-------------------------------------------
-# Server.DOMINIO / Ex.: DC.yourdomain.local
-
-$Domain = "DHCP-DEV-APP"
+#---- Declarar o dominio ---------------------
+# Server.DOMINIO
+$Domain = "YOUR-DHCP-SERVER"
 
 
 
@@ -40,15 +41,13 @@ function IpToDec($ip){
 
 $ipAddrDec = IpToDec($ipAddr)
 
-# Query to get IPs from IPAM database
-$Query = "SELECT `ipaddresses`.ip_addr, `ipaddresses`.mac, `ipaddresses`.description, `subnets`.subnet FROM `ipaddresses` INNER JOIN `subnets` ON `ipaddresses`.subnetId = `subnets`.id WHERE `ipaddresses`.subnetId = "+ $subnetId+" AND  `ipaddresses`.ip_addr= "+ $ipAddrDec+" "
-
-#database and credentials
-$MySQLAdminUserName = 'dhcpsync' 
-$MySQLAdminPassword = '********' 
-$MySQLDatabase = 'phpipam' 
-$MySQLHost = '192.168.1.100' 
-$ConnectionString = "server=" + $MySQLHost + ";port=3306;uid=" + $MySQLAdminUserName + ";pwd=" + $MySQLAdminPassword + ";database="+$MySQLDatabase 
+# Query e variaves de conexao
+$Query = "SELECT `ipaddresses`.ip_addr, `ipaddresses`.hostname, `ipaddresses`.mac, `ipaddresses`.description, `subnets`.subnet FROM `ipaddresses` INNER JOIN `subnets` ON `ipaddresses`.subnetId = `subnets`.id WHERE `ipaddresses`.subnetId = "+ $subnetId+" AND `ipaddresses`.ip_addr= "+ $ipAddrDec+" "
+$MySQLAdminUserName = '************' # DB username with select privileges
+$MySQLAdminPassword = '****************' # Password
+$MySQLDatabase = '*****' # DB name
+$MySQLHost = 'x.x.x.x' 
+$ConnectionString = "server=" + $MySQLHost + ";port=3307;uid=" + $MySQLAdminUserName + ";pwd=" + $MySQLAdminPassword + ";database="+$MySQLDatabase 
 
 
 
@@ -79,67 +78,90 @@ Try {
   $Connection = New-Object MySql.Data.MySqlClient.MySqlConnection 
   $Connection.ConnectionString = $ConnectionString 
   $Connection.Open() 
-  Write-Output "Running query..."
+  # # Write-Output "Running query..."
   $Command = New-Object MySql.Data.MySqlClient.MySqlCommand($Query, $Connection) 
   $DataAdapter = New-Object MySql.Data.MySqlClient.MySqlDataAdapter($Command) 
   $DataSet = New-Object System.Data.DataSet 
-  $RecordCount = $dataAdapter.Fill($dataSet, "data") 
+  $RecordCount = $dataAdapter.Fill($DataSet, "data") 
   $Records = $DataSet.Tables[0]
   $Records.Keys | ForEach-Object {$Records.$_}
-  Write-Output $Records
-
-  foreach ($reservation in $Records ){
-    
-    Write-Output "Start check..."
-    
-    #Setting Values
-    $ip_addr = $reservation.ip_addr
-    $subnet = $reservation.subnet
+  #Write-Output $Records
   
-    $ip_addr= [Convert]::ToString($ip_addr,2)
-    $subnet = [Convert]::ToString($subnet,2)
+  $res = @($Records).Length
+  #Write-Output $res
 
-    $ip =  IpFromBin($ip_addr)
-    $scope =  IpFromBin($subnet)
-
-    Write-Output $scope >> C:\ipam\log.txt
-    $mac = $reservation.mac
+  if($res -gt 0){
+      foreach ($reservation in $Records ){
     
-    $mac = [Convert]::ToString($mac)
-    $mac = $mac.Replace(':',"")
-    $mac = $mac.Replace('-',"")
-    $description = $reservation.description
-
-    #Check IP on DHCP
-    Write-Output "Check DHCP reservations.." >> C:\ipam\log.txt
-  
-    $checkReservations = 'Get-DhcpServerv4Reservation -ComputerName '+ $Domain + ' -IpAddress ' + $ip 
-    #Write-Output $checkReservations
-    $found = Invoke-Expression $checkReservations
-  
-    if(-not $found){
-        Write-Output "New reservation to load..." >> C:\ipam\log.txt
-        Write-Output "Loading dhcp Reservation"
-        Write-Output $ip >> C:\ipam\log.txt
-        $add =  'Add-DhcpServerv4Reservation -ScopeId '+ $scope+ ' -IpAddress '+ $ip +' -ClientId "'+ $mac +'" -Description "'+$description+'" '
-        Invoke-Expression $add | Tee-Object C:\ipam\log.txt
-    }  
+        #Write-Output "Start check..."
     
-    if($found){
-        Write-Output "Updating DHCP Reservation" >> C:\ipam\log.txt
-        Write-Output $ip >> C:\ipam\log.txt
-        $rm =  'Remove-DhcpServerv4Reservation -ComputerName '+ $Domain + ' -IpAddress '+ $ip +''
-        Invoke-Expression $rm | Tee-Object C:\ipam\log.txt
-        $add =  'Add-DhcpServerv4Reservation -ScopeId '+ $scope+ ' -IpAddress '+ $ip +' -ClientId "'+ $mac +'" -Description "'+$description+'" '
-        Invoke-Expression $add | Tee-Object C:\ipam\log.txt
-    }
+        #Setting Values
+        $ip_addr = $reservation.ip_addr
+        $subnet = $reservation.subnet
+  
+        $ip_addr= [Convert]::ToString($ip_addr,2)
+        $subnet = [Convert]::ToString($subnet,2)
+
+        $ip =  IpFromBin($ip_addr)
+        $scope =  IpFromBin($subnet)
+
+        Write-Output $scope >> C:\ipam\log.txt
+        $mac = $reservation.mac
+    
+        $mac = [Convert]::ToString($mac)
+        $mac = $mac.Replace(':',"")
+        $mac = $mac.Replace('-',"")
+        $dns_name = $reservation.hostname
+        $description = $reservation.description
+
+        #Check IP on DHCP
+        Write-Output "Check DHCP reservations:" >> C:\ipam\log.txt
+        Write-Output $dns_name >> C:\ipam\log.txt
+        Write-Output $description >> C:\ipam\log.txt
+
+        $checkReservations = 'Get-DhcpServerv4Reservation -IpAddress ' + $ip 
+
+        $remove_reservByMac = 'Remove-DhcpServerv4Reservation -ScopeId '+ $scope + ' -ClientId ' + $mac
+
+        Invoke-Expression $remove_reservByMac
+        
+
+        #Write-Output $checkReservations
+        $found = Invoke-Expression $checkReservations
+  
+        if(-not $found){
+            Write-Output "New reservation to load:" >> C:\ipam\log.txt
+            #Write-Output "Loading dhcp Reservation"
+            Write-Output $ip >> C:\ipam\log.txt
+            $add =  'Add-DhcpServerv4Reservation -ScopeId '+ $scope+ ' -IpAddress '+ $ip +' -ClientId "'+ $mac +'" -Name "'+ $dns_name +'" -Description "'+$description+'" '
+            Invoke-Expression $add | Tee-Object C:\ipam\log.txt
+            Write-Output "IP reservations OK." >> C:\ipam\log.txt
+        }  
+    
+        if($found){
+            Write-Output "Updating DHCP Reservation" >> C:\ipam\log.txt
+            Write-Output $ip >> C:\ipam\log.txt
+            $rm =  'Remove-DhcpServerv4Reservation -ComputerName '+ $Domain + ' -IpAddress '+ $ip +''
+            Invoke-Expression $rm | Tee-Object C:\ipam\log.txt
+            $add =  'Add-DhcpServerv4Reservation -ScopeId '+ $scope+ ' -IpAddress '+ $ip +' -ClientId "'+ $mac +'" -Name "'+ $dns_name +'" -Description "'+$description+'" '
+            Invoke-Expression $add | Tee-Object C:\ipam\log.txt
+            Write-Output "IP reservations OK." >> C:\ipam\log.txt
+        }
+      }
   }
-
-  Write-Output " . " >> C:\ipam\log.txt
-
-    
-  Write-Output "Finish." >> C:\ipam\log.txt
-
+  else{
+    Write-Output "Removing DHCP Reservation" >> C:\ipam\log.txt
+    Write-Output $ipAddr >> C:\ipam\log.txt
+    $rm =  'Remove-DhcpServerv4Reservation -ComputerName '+ $Domain + ' -IpAddress '+ $ipAddr +''
+    Invoke-Expression $rm | Tee-Object C:\ipam\log.txt           
+  }
+  
+  # If you are using Failover DHCP
+  #$replication = 'Invoke-DhcpServerv4FailoverReplication -force'
+  #Invoke-Expression $replication 
+  #Write-Output "Failover Replication - Success" >> C:\ipam\log.txt
+ 
+  Write-Output "--------------------------END------------------------------" >> C:\ipam\log.txt
 } 
 Catch { 
     
@@ -148,4 +170,4 @@ Catch {
 
 Finally { 
   $Connection.Close() 
-}
+} 
